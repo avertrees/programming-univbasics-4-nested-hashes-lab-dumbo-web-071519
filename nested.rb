e4d35018b314001b2ea417456f28dec2350c0c59
Alessandra Vertrees <avertrees447@gmail.com>
2019-06-18 16:26:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -84,7 +85,7 @@ def adding_matz
     :known_for => "Ruby",
     :languages => ["LISP", "C"] 
   }
-  return programmer_hash[]
+  return programmer_hash
 end
 
 def changing_alan
@@ -105,7 +106,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alans_kay][:known_for] = alans_new_info
      
 end
 
